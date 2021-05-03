51f390a73108c7b3651c0d4048da5afe2eaee2c9
Muhammad <hablim51@gmail.com>
2021-05-03 20:48:44 +0700

    Memproses Data Internet Sebagai JSON

--- app/src/main/java/Hewan.kt
+++ app/src/main/java/Hewan.kt
@@ -1,5 +1,8 @@
+import org.d3if4129.galerihewan2.R
+
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+//    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 )
